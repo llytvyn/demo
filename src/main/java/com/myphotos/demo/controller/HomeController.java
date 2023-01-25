98532c7e6a19041ab9632c289cadb3edbf79d93e
HP-PC\HP ENVY 15-J108EL <freshdingo@zoho.com>
2023-01-25 11:23:48 +0100

    Add files

---
+++ src/main/java/com/myphotos/demo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.myphotos.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/") // questo metodo corrisponde a questo path
+    @ResponseBody // tutto il contenuto sarà mostrato all'utente
+    public String index(){
+        return "Hello World!";
+    }
+    @RequestMapping("/second")
+    @ResponseBody
+    public String secondPage() {
+        return "This is a second page!";
+    }
+}
